bc4cbd8284608430f4ea8be2eeb8f706a910b482
Ifescohub <101941367+Ifescohub@users.noreply.github.com>
2022-07-10 15:52:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
 const charts = $("[class*='chart-']");
 const chartAmount = $(".chart-amount");
+const chartDay = $(".chart-day");
+
+
+//Fetching data from JSON file
+fetch("./data.json")
+.then(response => {
+    return response.json();
+})
 
 // Dispaly chart amount on chart stick hover
-
-charts.hover(function(){ 
-    $(this).find(chartAmount).toggleClass("active");
+.then(data => {
+    charts.hover(function(){ 
+        for(i=0; i<data.length; i++){
+        $(this).find("#"+[i]).html("$"+data[i].amount);
+        $(this).find(chartAmount).toggleClass("active");
+        }
+      });
 });
-
-
